a3ef26b282bb37c3f2151d8e2a1bd8bab24e7c2d
Adil Gahramanov <adil.qehremanov.2000@gmail.com>
2025-12-17 19:37:36 +0000

    correction of the data to match 2018-2019

--- models/example/station_dimension.sql
+++ models/example/station_dimension.sql
@@ -6,7 +6,7 @@ START_STATIO_ID AS STATION_ID,
 start_station_name AS STATION_NAME, 
 start_lat AS STATION_LAT, 
 start_lng AS START_STATION_LNG
-FROM {{ source('demo', 'bike') }}
+FROM {{ ref('stg_bike') }}
 WHERE RIDE_ID != 'ride_id'
 
 )
